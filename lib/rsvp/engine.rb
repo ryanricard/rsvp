5b4f9a742515be8e3cc637e70e30e8914ae404d6
Ryan Ricard <ryan.ricard@gmail.com>
2014-01-20 20:51:15 -0600

    Added initializer to Rsvp to append engine db/migration path to the paths of the wrapping application. This way the engine’s migrations execute without needing to be copied to the wrapping application.

--- lib/rsvp/engine.rb
+++ lib/rsvp/engine.rb
@@ -5,5 +5,16 @@ module Rsvp
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    # Add engine db/migrate to paths so migrations run with wrapping application
+    # Inspired by: http://pivotallabs.com/leave-your-migrations-in-your-rails-engines/
+    initializer :append_migrations do |app|
+      # unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      # end
+    end
+
   end
 end
